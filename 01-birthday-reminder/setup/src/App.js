19e2ea9622357ee7879d9a08c5221afb3f26796b
ali <ali.alameldin98@gmail.com>
2021-09-15 19:41:05 +0200

    first commit

---
+++ 01-birthday-reminder/setup/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  return <h2>reminder project setup</h2>;
+}
+
+export default App;
+
+// kol object 3ando id w name w age w image
